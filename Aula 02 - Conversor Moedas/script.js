2574cf091db1149ad3c0fcd58c4c20f886ddd081
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2021-09-14 16:19:21 -0300

    Initial Commit
    Inicio da aula 02

---
+++ Aula 02 - Conversor Moedas/script.js
@@ -0,0 +1,6 @@
+function Converter() {
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    valorEmDolarNumerico = parseFloat(valor)
+    console.log(valorEmDolarNumerico);
+}
