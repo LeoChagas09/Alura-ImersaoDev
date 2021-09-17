83ef06ab8ce8a0bf4abaffafddd2426af0cdfd70
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2021-09-17 15:41:29 -0300

    Initial commit
    Final da Aula 04

---
+++ Aula 04 - Array, lista e AluraFlix/script.js
@@ -0,0 +1,10 @@
+var listaFilmes = [
+    "https://br.web.img3.acsta.net/c_310_420/pictures/21/04/14/19/06/3385237.jpg",
+    "https://br.web.img2.acsta.net/pictures/19/07/05/17/30/5167951.jpg",
+    "https://br.web.img3.acsta.net/pictures/19/04/26/17/30/2428965.jpg",
+    
+];
+
+for(var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">")
+}
