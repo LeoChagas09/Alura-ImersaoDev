2e7f21f49bc134787717992261c95361fddf3376
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2021-09-17 09:47:40 -0300

    Initial commit
    Fim da Aula 02 - Alura

--- Aula 02 - Conversor Moedas/script.js
+++ Aula 02 - Conversor Moedas/script.js
@@ -1,6 +1,12 @@
 function Converter() {
     var valorElemento = document.getElementById("valor");
     var valor = valorElemento.value;
-    valorEmDolarNumerico = parseFloat(valor)
-    console.log(valorEmDolarNumerico);
+    var valorEmDolarNumerico = parseFloat(valor)
+    
+    var valorEmReal = valorEmDolarNumerico * 5
+    console.log(valorEmReal)
+
+    var elementoValorConvertido = document.getElementById("valorConvertido")
+    var valorConvertido = "O resultado em real é R$ " + valorEmReal;
+    elementoValorConvertido.innerHTML = valorConvertido;
 }
